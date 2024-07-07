756c262994b0c9cb70db55d2b54f123c8a51eadc
Masudur400 <md8004210@gmail.com>
2024-07-07 20:09:20 +0600

    navbar add

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../NavBar/NavBar";
+
+ 
+const Root = () => {
+    return (
+        <div>
+            <div className="container mx-auto">
+            <NavBar></NavBar>
+            </div>
+            <div className="container mx-auto px-4">
+            <Outlet></Outlet>
+            </div>
+        </div>
+    );
+};
+
+export default Root;
