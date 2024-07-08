6c949abf310daf2dce713bb890bbfb135cf46be8
Masudur400 <md8004210@gmail.com>
2024-07-08 18:02:30 +0600

    create form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,24 @@
- 
+import { Link } from "react-router-dom";
+
 const Login = () => {
     return (
-        <div>
-            Login
+        <div className="lg:w-1/3 md:w-1/2 mx-auto my-14 p-5 rounded-lg bg-orange-100 shadow-md">
+            <h3 className="text-3xl font-bold text-center text-orange-500 my-4">Please LogIn</h3>
+            <form className="space-y-5">
+                 
+                <div>
+                    <input type="email" name="email" placeholder="Your Email" id="" className="w-full px-4 py-2 rounded-md" />
+
+                </div>
+                <div>
+                    <input type="password" name="password" placeholder="Password" id="" className="w-full px-4 py-2 rounded-md" />
+                </div>
+                
+                <div>
+                    <input type="submit" value="LogIn" className="w-full px-4 py-2 rounded-md bg-orange-500 text-white font-bold hover:bg-orange-600" />
+                </div>
+            </form>
+            <p className="my-3">Do not have an account <Link to='/register' className="text-red-500 font-bold">Please Register</Link></p>
         </div>
     );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,29 @@
+import { Link } from "react-router-dom";
+
  
 const Register = () => {
     return (
-        <div>
-            register
+        <div className="lg:w-1/3 md:w-1/2 mx-auto my-14 p-5 rounded-lg bg-orange-100 shadow-md">
+            <h3 className="text-3xl font-bold text-center text-orange-500 my-4">Please Register</h3>
+            <form className="space-y-5">
+                <div>
+                    <input type="text" name="name" placeholder="Your Name" id="" className="w-full px-4 py-2 rounded-md" />
+                </div>
+                <div>
+                    <input type="email" name="email" placeholder="Your Email" id="" className="w-full px-4 py-2 rounded-md" />
+
+                </div>
+                <div>
+                    <input type="password" name="password" placeholder="Password" id="" className="w-full px-4 py-2 rounded-md" />
+                </div>
+                <div>
+                    <input type="file" name="photo" id="" className="w-full px-4 py-2 rounded-md" />
+                </div>
+                <div>
+                    <input type="submit" value="Register" className="w-full px-4 py-2 rounded-md bg-orange-500 text-white font-bold hover:bg-orange-600" />
+                </div>
+            </form>
+            <p className="my-3">Already an account <Link to='/login' className="text-red-500 font-bold">Please Login</Link></p>
         </div>
     );
 };
