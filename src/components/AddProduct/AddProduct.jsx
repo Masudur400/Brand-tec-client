c08f12f97e671410feb5bd71f364ec4c24152ed6
Masudur400 <md8004210@gmail.com>
2024-07-13 20:00:41 +0600

    product add to server site

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -1,25 +1,74 @@
+import axios from "axios";
 import { Helmet } from "react-helmet";
+import useAxiosSecure from "../Hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
+
+
+
+const imageHostingKey = import.meta.env.VITE_image_hosting_key;
+const imageHostingApi = `https://api.imgbb.com/1/upload?key=${imageHostingKey}`;
 
 const AddProduct = () => {
 
-    const handleAddProduct = e =>{
+    const axiosSecure = useAxiosSecure()
+
+    const handleAddProduct = async (e) => {
         e.preventDefault()
         const form = new FormData(e.currentTarget)
-       const  productName = form.get('productName')
-       const productBrand = form.get('productBrand')
-       const oldPrice = form.get('oldPrice')
-       const newPrice = form.get('newPrice')
-       const productQuantity = form.get('productQuantity')
-       const productImage = form.get('productImage')
-       const productDetails = form.get('productDetails')
+        const productName = form.get('productName')
+        const productBrand = form.get('productBrand')
+        const oldPrice = form.get('oldPrice')
+        const newPrice = form.get('newPrice')
+        const productQuantity = form.get('productQuantity')
+        const photoFile = form.get('productImage')
+        const productDetails = form.get('productDetails')
 
-       console.table(productName, productBrand, oldPrice, newPrice, productQuantity, productImage, productDetails)
-        
+        // console.log(productName, productBrand, oldPrice, newPrice, productQuantity, photoFile, productDetails)
+
+        try {
+            const imageData = new FormData();
+            imageData.append('image', photoFile);
+
+            const imageRes = await axios.post(imageHostingApi, imageData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+
+            const imageUrl = imageRes.data.data.url;
+
+            const data = {
+                productName,
+                productBrand,
+                oldPrice,
+                newPrice,
+                productQuantity,
+                productImage: imageUrl,
+                productDetails
+            }
+ 
+
+            axiosSecure.post('/products', data)
+                .then(res => {
+                    if (res.data.insertedId) {
+                        Swal.fire({
+                            title: "Success!",
+                            text: "Product added successfully!",
+                            icon: "success"
+                        });
+                    }
+                    
+                }) 
+
+        } catch (error) {
+            console.error('Error uploading the image or submitting the form:', error);
+        }
+
     }
 
     return (
-        <div> 
+        <div>
             <div className="lg:w-2/4 md:w-2/3 mx-auto my-5 md:p-5 p-3 rounded-lg bg-gradient-to-r from-orange-200 to-red-200 shadow-md max-sm:mx-4 ">
                 <Helmet>
                     <title>Login</title>
@@ -29,39 +78,39 @@ const AddProduct = () => {
                 <form onSubmit={handleAddProduct}>
 
                     <div className="grid grid-cols-1 md:grid-cols-2 md:gap-3">
-                    <div>
-                        <p className="font-semibold">Product Name</p>
-                        <input type="text" name="productName" placeholder="Product Name" id="" className="border-2 rounded-md w-full text-sm md:text-base px-4 md:py-1 mb-2" /> 
+                        <div>
+                            <p className="font-semibold">Product Name</p>
+                            <input type="text" name="productName" placeholder="Product Name" id="" className="border-2 rounded-md w-full text-sm md:text-base px-4 md:py-1 mb-2" />
+                        </div>
+                        <div>
+                            <p className="font-semibold">Product Brand</p>
+                            <input type="text" name="productBrand" placeholder="Product Brand" id="" className="border-2 rounded-md w-full text-sm md:text-base px-4 md:py-1 mb-2" />
+                        </div>
+                        <div>
+                            <p className="font-semibold">Old Price</p>
+                            <input type="text" name="oldPrice" placeholder="Old Price" id="" className="border-2 rounded-md w-full text-sm md:text-base px-4 md:py-1 mb-2" />
+                        </div>
+                        <div>
+                            <p className="font-semibold">New Price</p>
+                            <input type="text" name="newPrice" placeholder="New Price" id="" className="border-2 rounded-md w-full text-sm md:text-base px-4 md:py-1 mb-2" />
+                        </div>
+                        <div>
+                            <p className="font-semibold">Product Quantity</p>
+                            <input type="text" name="productQuantity" placeholder="Product Quantity" id="" className="border-2 rounded-md w-full text-sm md:text-base px-4 md:py-1 mb-2" />
+                        </div>
+                        <div>
+                            <p className="font-semibold text-sm md:text-base">Product Image</p>
+                            <input type="file" placeholder="" name="productImage" id="" className="border-2 rounded-sm md:rounded-md w-full text-sm md:text-base  mb-2 bg-white" />
+                        </div>
                     </div>
                     <div>
-                        <p className="font-semibold">Product Brand</p>
-                        <input type="text" name="productBrand" placeholder="Product Brand" id="" className="border-2 rounded-md w-full text-sm md:text-base px-4 md:py-1 mb-2" /> 
-                    </div>
-                    <div>
-                        <p className="font-semibold">Old Price</p>
-                        <input type="text" name="oldPrice" placeholder="Old Price" id="" className="border-2 rounded-md w-full text-sm md:text-base px-4 md:py-1 mb-2" /> 
-                    </div>
-                    <div>
-                        <p className="font-semibold">New Price</p>
-                        <input type="text" name="newPrice" placeholder="New Price" id="" className="border-2 rounded-md w-full text-sm md:text-base px-4 md:py-1 mb-2" /> 
-                    </div>
-                    <div>
-                        <p className="font-semibold">Product Quantity</p>
-                        <input type="text" name="productQuantity" placeholder="Product Quantity" id="" className="border-2 rounded-md w-full text-sm md:text-base px-4 md:py-1 mb-2" /> 
-                    </div>
-                    <div>
-                    <p className="font-semibold text-sm md:text-base">Product Image</p>
-                    <input type="file" placeholder="" name="productImage" id="" className="border-2 rounded-sm md:rounded-md w-full text-sm md:text-base  mb-2 bg-white" />
-                    </div>
-                    </div>
-                    <div>
-                    <p className="font-semibold text-sm md:text-base">Product Details</p>
-                   <textarea name="productDetails" placeholder="Product Details" id="" className="border-2 rounded-md w-full text-sm md:text-base px-4 md:py-1 mb-2"></textarea>
+                        <p className="font-semibold text-sm md:text-base">Product Details</p>
+                        <textarea name="productDetails" placeholder="Product Details" id="" className="border-2 rounded-md w-full text-sm md:text-base px-4 md:py-1 mb-2"></textarea>
                     </div>
 
 
-                    <div className="flex justify-center"> 
-                        <input className="w-fit px-4 py-1 md:py-2 text-center text-lg rounded-md bg-gradient-to-r from-orange-500 to-red-500 hover:from-red-400 hover:to-orange-400 text-white font-bold my-3" type="submit" value="Add Product" /> 
+                    <div className="flex justify-center">
+                        <input className="w-fit px-4 py-1 md:py-2 text-center text-lg rounded-md bg-gradient-to-r from-orange-500 to-red-500 hover:from-red-400 hover:to-orange-400 text-white font-bold my-3" type="submit" value="Add Product" />
 
                     </div>
                 </form>
